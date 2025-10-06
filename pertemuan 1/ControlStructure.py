c1803a742e66651115bece9384771c9402e05697
nauflabhy <n4uf4l.ku@gmail.com>
2025-10-06 15:05:28 +0700

    Fungsi input persentase nilai siswa

---
+++ pertemuan 1/ControlStructure.py
@@ -0,0 +1,9 @@
+#    If % is >=90 then Excellent performance
+
+#    If % is >=80 then  Very Good performance
+
+#    If % is >=70 then Good performance
+
+#    If % is >=60 then average performance
+
+percentage = float(input("Masukan persentase nilai siswa: "))
